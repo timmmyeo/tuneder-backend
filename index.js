1ec69f06c3bc5344228d89de804911ad35bf3b55
Jordan-Tay <tayjordan25@gmail.com>
2021-03-28 18:21:27 +0800

    add whitelist

--- index.js
+++ index.js
@@ -15,7 +15,19 @@ firebase.initializeApp({
 
 var db = firebase.firestore();
 
-app.use(cors());
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://tuneder.netlify.app/", "http://localhost:3000"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("IC HELLO WORLD"));
